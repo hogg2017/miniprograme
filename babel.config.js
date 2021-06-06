130fb40298da5909dea7271254dc8aae7010760e
hogger <deuntlt@vip.qq.com>
2021-06-06 16:01:18 +0800

    测试一下提交情况

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-module.exports = { //
+module.exports = { //测试一下
   presets: [
     ['taro', {
       framework: 'vue',
